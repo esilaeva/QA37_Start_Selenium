f2abdc773b5552b41496ef224fffabd2282c074f
esilaeva <emsilaeva@gmail.com>
2023-03-13 20:11:39 +0200

    13/03/23

--- src/test/java/Homeworks.java
+++ src/test/java/Homeworks.java
@@ -121,4 +121,44 @@ public class Homeworks {
         WebElement divElContains1 = wd.findElement(By.xpath("//*[contains(@placeholder, 'ai')]"));
 
     }
+
+    @Test
+    public void classwork(){
+        //parent
+        WebElement el = wd.findElement(By.xpath("//h1/parent::*"));
+        WebElement el1 = wd.findElement(By.xpath("//h1/parent::div"));
+        WebElement el2 = wd.findElement(By.xpath("//h1/.."));
+
+        //ancestor
+        WebElement el3 = wd.findElement(By.xpath("//h1/ancestor::*")); //all
+        WebElement el4 = wd.findElement(By.xpath("//h1/ancestor::div")); // two options
+        WebElement el5 = wd.findElement(By.xpath("//h1/ancestor::div[2]")); //one option
+
+        // ancestor and self (plus one)
+        WebElement el6 = wd.findElement(By.xpath("//h1/ancestor-or-self::*"));
+        List<WebElement> list = wd.findElements(By.xpath("//h1/ancestor-or-self::*"));
+
+        // following-sibling
+        List<WebElement> list1 = wd.findElements(By.xpath("//h1/following-sibling::a"));
+
+        //preceding-sibling
+        WebElement el7= wd.findElement(By.xpath("//a[@href='/login']/preceding-sibling::h1"));
+        List<WebElement> list2 = wd.findElements(By.xpath("//a[@href='/login']/preceding-sibling::a"));
+   }
+
+    @Test
+    public void classwork2(){
+        WebElement element = wd.findElement(By.cssSelector("[name='login']"));
+
+        String text = element.getText(); // get from innerText
+        System.out.println(text);
+
+        WebElement form = wd.findElement(By.xpath("//form"));
+        String testForm = form.getText();
+        System.out.println("**********");
+        System.out.println(testForm);
+
+
+    }
+
 }
